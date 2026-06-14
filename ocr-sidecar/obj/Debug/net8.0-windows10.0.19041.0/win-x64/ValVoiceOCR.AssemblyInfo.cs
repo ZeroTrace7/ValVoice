e190e91a0d47d89dcc54b9ff22bc1b5188ddc8a3
ZeroTrace7 <shreyashgupta999@gmail.com>
2026-06-14 14:01:38 +0530

    restore working OCR baseline before segmentation work

--- ocr-sidecar/obj/Debug/net8.0-windows10.0.19041.0/win-x64/ValVoiceOCR.AssemblyInfo.cs
+++ ocr-sidecar/obj/Debug/net8.0-windows10.0.19041.0/win-x64/ValVoiceOCR.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ValVoiceOCR")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe6ccee5f56f6dc05de711c8d7a980214caf95aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ValVoiceOCR")]
